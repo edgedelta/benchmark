d083c1d420ce23aabe3df71a80b3f7b0e1df2410
asimerel <44316960+asimerel@users.noreply.github.com>
2026-06-09 10:40:42 +0300

    Auto-detect latest otelcol-contrib version at install time (#9)

--- scripts/install_agent_otelcol.sh
+++ scripts/install_agent_otelcol.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-otelcol_version="0.153.0"
+otelcol_version=$(curl -sSL "https://api.github.com/repos/open-telemetry/opentelemetry-collector-releases/releases/latest" \
+  | jq -r '.tag_name | ltrimstr("v")')
 
 curl -sSL -o otelcol-contrib.deb \
   "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${otelcol_version}/otelcol-contrib_${otelcol_version}_linux_amd64.deb"
